38aed38fb6b61bff8d1f054d1e2693f5708189f7
Sasha <azhiburtovich@xbsoftware.com>
2012-11-28 00:08:11 +0300

    работа над доской

--- checkers.js
+++ checkers.js
@@ -1,9 +1,16 @@
 if (Meteor.isClient) {
-  Template.body.created = function() {
-      document.body.appendChild(Template.board());
-  };
   Template.board.setCell = [
             {x:20,y:20,fill:"black"},
             {x:50,y:20,fill:"none"}
         ];
+	Template.board.appendCell = function () {
+		var ns = "http://www.w3.org/2000/svg";
+		var cell = document.createElementNS(ns,"rect");
+		cell.setAttribute("x",this.x);
+		cell.setAttribute("y",this.y);
+		cell.setAttribute("width",30);
+		cell.setAttribute("height",30);
+		cell.setAttribute("fill",this.fill);
+		$("<svg>").append(cell);
+	};
 }
