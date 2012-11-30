962c54bce3bce0bded0fbc93e32e9355f0031f82
Sasha <azhiburtovich@xbsoftware.com>
2012-11-30 23:59:17 +0300

    svg version

--- checkers.js
+++ checkers.js
@@ -1,10 +1,16 @@
 Checkers = new Meteor.Collection("checkers");
 if (Meteor.is_client) {
+	Meteor.autosubscribe(function(){
+		var _checkers = Checkers.find({}).fetch();
+		$("svg>circle").remove();
+		for(var i=0; i<_checkers.length; i++){
+			$("svg").append(createChecker.apply(_checkers[i]));
+		};
+	});
     Template.board.rendered = function () {
         appendCell(0,0);
-		//appendCheckers();
+		//var curcor = Checkers.find({});
     };
-	Meteor.render(appendCheckers);
     function appendCell(i,j){
         var cell;
         if (i < 8) {
@@ -31,28 +37,14 @@ if (Meteor.is_client) {
         cell.setAttribute("height",50);
         cell.setAttribute("fill",this.fill);
         return cell;
-    }
-	function appendCheckers(){
-		var _checkers = Checkers.find({}).fetch();
-		for(var i=0; i<_checkers.linght; i++){
-			$("svg").append(createChecker.append(_checkers[i]));
-		};
-	};
+    };
 	function createChecker(){
 		var ns = "http://www.w3.org/2000/svg";
         var checker = document.createElementNS(ns,"circle");
         checker.setAttribute("cx",31+this.v*50);
 		checker.setAttribute("cy",31+this.h*50);
-		checker.setAttribute("r",40);
+		checker.setAttribute("r",20);
 		checker.setAttribute("fill",this.color);
 		return checker;
 	}
-	/*function insertCheckrs () {
-		for(var i=0; i<24; i++){
-			Checkers.insert({
-				v : (i==0)?0:
-					(i==8)?
-			});
-		}
-	}*/
 }
