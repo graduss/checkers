31856d0cb7684ee7b6d24b93cd6c4f28c873a89f
Sasha <azhiburtovich@xbsoftware.com>
2012-12-02 15:27:37 +0300

    create checker

--- checkers.js
+++ checkers.js
@@ -6,12 +6,20 @@ if (Meteor.is_client) {
 			rows.push({rowNum:i})
 		}
 		return rows;
-	}
+	};
 	Template.row.cells = function(){
+		var check = Checkers.find({}).fetch();
 		var cells = [];
 		for(var i=1; i<=8; i++){
-			cells.push({background:(Math.pow(-1,i+this.rowNum)>0)?"whiteCell":"blackCell"});
+			cells.push({background: (Math.pow(-1,i+this.rowNum)>0)?"whiteCell":"blackCell",
+						check: seachChecker(this.rowNum,i,check)});
 		}
 		return cells;
-	}
+	};
+	function seachChecker(v,h,checkers){
+		for(var i=0; i<checkers.length; i++){
+			if(checkers[i].v == v && checkers[i].h == h) return checkers[i];
+		}
+		return null;
+	};
 }
