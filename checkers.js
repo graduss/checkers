b60ded134aa6e3a87367e3852e140ed05de6f852
«Zhiburtovich <azhiburtovich@xbsoftware.com>
2012-12-05 11:03:55 +0300

    add setect funtion

--- checkers.js
+++ checkers.js
@@ -16,17 +16,37 @@ if (Meteor.is_client) {
 		}
 		return cells;
 	};
-        Template.checker.select = function(){
+        /*Template.checker.select = function(){
             return Session.equals("select",this._id)?"border: 1px solid coral;":"";
-        };
+        };*/
         Template.checker.events = {
             "mousedown" : function(){
-                Session.set("select", this._id);
+                select.apply(this);
+                //Session.set("select", this._id);
             },
             "mouseup" : function(){
-                Session.set("select", null);
+                //Session.set("select", null);
             }
         };
+        function select(){
+            var checker = $("#"+this._id);
+            var offset = checker.offset();
+            var Geometry = {
+                width : checker.width(),
+                height : checker.height(),
+                top : offset.top,
+                left : offset.left,
+                setGeometry : function(checker){
+                    checker.css("position","absolute");
+                    checker.width(this.width);
+                    checker.height(this.height);
+                    checker.css("top",this.top);
+                    checker.css("left",this.left);
+                }
+            };
+            $('body').append($("#"+this._id).get(0));
+            Geometry.setGeometry(checker);
+        }
 	function seachChecker(v,h,checkers){
 		for(var i=0; i<checkers.length; i++){
 			if(checkers[i].v == v && checkers[i].h == h) return checkers[i];
